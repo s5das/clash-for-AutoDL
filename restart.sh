3cc1ff448cc29f0ba893f69da173476c6873071b
vocabvictos <wuzhongheng@smail.nju.edu.cn>
2024-06-28 11:23:03 +0000

    修复重启异常

--- restart.sh
+++ restart.sh
@@ -28,7 +28,7 @@ action() {
 # 函数，判断命令是否正常执行
 if_success() {
   local ReturnStatus=$3
-  if [ $ReturnStatus -eq 0 ]; then
+  if [ "$ReturnStatus" -eq 0 ]; then
           action "$1" /bin/true
   else
           action "$2" /bin/false
@@ -36,7 +36,7 @@ if_success() {
   fi
 }
 
-# 定义路劲变量
+# 定义路径变量
 Server_Dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 Conf_Dir="$Server_Dir/conf"
 Log_Dir="$Server_Dir/logs"
@@ -45,46 +45,46 @@ Log_Dir="$Server_Dir/logs"
 Text1="服务关闭成功！"
 Text2="服务关闭失败！"
 # 查询并关闭程序进程
-PID_NUM=`ps -ef | grep [c]lash-linux | wc -l`
-PID=`ps -ef | grep [c]lash-linux | awk '{print $2}'`
-if [ $PID_NUM -ne 0 ]; then
-	kill $PID &>/dev/null
+PID_NUM=$(ps -ef | grep [c]lash-linux | wc -l)
+PID=$(ps -ef | grep [c]lash-linux | awk '{print $2}')
+ReturnStatus=0
+if [ "$PID_NUM" -ne 0 ]; then
+  kill "$PID" &>/dev/null
   ReturnStatus=$?
-	# ps -ef | grep [c]lash-linux-a | awk '{print $2}' | xargs kill -9
+  # ps -ef | grep [c]lash-linux-a | awk '{print $2}' | xargs kill -9
 fi
-if_success $Text1 $Text2 $ReturnStatus
+if_success "$Text1" "$Text2" "$ReturnStatus"
 
 sleep 3
 
 ## 获取CPU架构
 if /bin/arch &>/dev/null; then
-	CpuArch=`/bin/arch`
+  CpuArch=$(/bin/arch)
 elif /usr/bin/arch &>/dev/null; then
-	CpuArch=`/usr/bin/arch`
+  CpuArch=$(/usr/bin/arch)
 elif /bin/uname -m &>/dev/null; then
-	CpuArch=`/bin/uname -m`
+  CpuArch=$(/bin/uname -m)
 else
-	echo -e "\033[31m\n[ERROR] Failed to obtain CPU architecture！\033[0m"
-	exit 1
+  echo -e "\033[31m\n[ERROR] Failed to obtain CPU architecture！\033[0m"
+  exit 1
 fi
 
 ## 重启启动clash服务
 Text5="服务启动成功！"
 Text6="服务启动失败！"
 if [[ $CpuArch =~ "x86_64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
-	ReturnStatus=$?
-	if_success $Text5 $Text6 $ReturnStatus
+  nohup "$Server_Dir/bin/clash-linux-amd64" -d "$Conf_Dir" &> "$Log_Dir/clash.log" &
+  ReturnStatus=$?
+  if_success "$Text5" "$Text6" "$ReturnStatus"
 elif [[ $CpuArch =~ "aarch64" ||  $CpuArch =~ "arm64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-arm64 -d $Conf_Dir &> $Log_Dir/clash.log &
-	ReturnStatus=$?
-	if_success $Text5 $Text6 $ReturnStatus
+  nohup "$Server_Dir/bin/clash-linux-arm64" -d "$Conf_Dir" &> "$Log_Dir/clash.log" &
+  ReturnStatus=$?
+  if_success "$Text5" "$Text6" "$ReturnStatus"
 elif [[ $CpuArch =~ "armv7" ]]; then
-	nohup $Server_Dir/bin/clash-linux-armv7 -d $Conf_Dir &> $Log_Dir/clash.log &
-	ReturnStatus=$?
-	if_success $Text5 $Text6 $ReturnStatus
+  nohup "$Server_Dir/bin/clash-linux-armv7" -d "$Conf_Dir" &> "$Log_Dir/clash.log" &
+  ReturnStatus=$?
+  if_success "$Text5" "$Text6" "$ReturnStatus"
 else
-	echo -e "\033[31m\n[ERROR] Unsupported CPU Architecture！\033[0m"
-	exit 1
+  echo -e "\033[31m\n[ERROR] Unsupported CPU Architecture！\033[0m"
+  exit 1
 fi
-
